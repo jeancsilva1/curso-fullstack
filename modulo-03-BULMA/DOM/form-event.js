b2dc814f6054dbd7ff0c636e33e2350b2dd74e85
Jean C Silva <jesilva456@hotmail.com>
2022-05-14 13:44:24 -0300

    remove duplicated folder

--- modulo-03-BULMA/DOM/form-event.js
+++
@@ -1,6 +0,0 @@
-function save(){
-    event.preventDefault()
-    let name = document.querySelector("input[name='name']").value
-let programmingLang = document.querySelector("select[name='programming-lang']").value
-alert("Nome: " + name + "\nLinguagem de Programação: " + programmingLang)
-}
